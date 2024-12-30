5591df807e19314a6a0b72b510dc292df3188289
Fattahq06 <ihsanfattah577@gmail.com>
2024-12-30 22:52:46 +0700

    test database

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+//INSERT INTO `kontak` (`id`, `email`, `pesan`) VALUES (NULL, 'email', 'pesan');
+$conn = mysqli_connect("localhost", "u594045982_user", "Nookstudio#404", "u594045982_NookStudio");
+
+if( isset($_POST["submit"])){
+    $email = $_POST["email"];
+    $pesan = $_POST["pesan"];
+// INSERT INTO `kontak` (`id`, `email`, `pesan`) VALUES (NULL, 'fattah', 'ihsan')
+mysqli_query($conn, "INSERT INTO `kontak` (`id`, `email`, `pesan`) VALUES (NULL, '$email', '$pesan')");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
